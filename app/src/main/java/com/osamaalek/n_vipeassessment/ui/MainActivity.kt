848c56e91f7e2852f3a2b1ac0901e266274abde5
Osama Alek <osamaalek99@gmail.com>
2024-07-27 02:54:55 +0300

    Navigation
    -Added main Nav
    -Added direction nav

--- app/src/main/java/com/osamaalek/n_vipeassessment/ui/MainActivity.kt
+++ app/src/main/java/com/osamaalek/n_vipeassessment/ui/MainActivity.kt
@@ -1,20 +1,22 @@
-package com.osamaalek.n_vipeassessment
+package com.osamaalek.n_vipeassessment.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.osamaalek.n_vipeassessment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nav_host_fragment)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
 }
