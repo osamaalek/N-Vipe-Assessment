b183fcaae62f5a8cd471b45b51c24715fa1de90d
Osama Alek <osamaalek99@gmail.com>
2024-07-27 02:52:05 +0300

    UI
    -Added directions fragment
    -Added map fragment

---
+++ app/src/main/java/com/osamaalek/n_vipeassessment/ui/DirectionsFragment.kt
@@ -0,0 +1,19 @@
+package com.osamaalek.n_vipeassessment.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.osamaalek.n_vipeassessment.R
+
+class DirectionsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_directions, container, false)
+    }
+}
